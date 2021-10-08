b7d3cff9d423f5993ec4b74365fd15f6b9f4de1c
Hanfeng <hanfengwebdesign@gmail.com>
2021-10-08 23:39:57 +0800

    321

--- src/components/Workbar/ChatDisplay.tsx
+++ src/components/Workbar/ChatDisplay.tsx
@@ -1,9 +1,10 @@
-import { ReactElement, useContext } from "react"
+import { ReactElement, useContext, useEffect, useRef } from "react"
 import ChatBox from './ChatBox'
 import { ActionType, AppContext, AppState, ChatStreamItem } from "../../store"
 interface IProps{
     children?:ReactElement,
-    chatStream:ChatStreamItem[]
+    chatStream:ChatStreamItem[],
+    getDisplayer:(item:any)=>void
 }
 
 const ChatDisplay = (props:IProps)=>{
@@ -11,10 +12,14 @@ const ChatDisplay = (props:IProps)=>{
     const goback = ()=>{
         dispatch({type:ActionType.CHANGE_ACTIVED,payload:''})
     }
+    const displayer = useRef(null)
+    useEffect(()=>{
+        props.getDisplayer(displayer)
+    },[props.chatStream])
     return(
-        <div className=''>
+        <div>
             <div className='border-b border-gray-100 pb-2 text-gray-300  text-xs  text-right'> <span className=' cursor-pointer' onClick={goback}>{'< 返回'}</span> </div>
-            <div className='display-container h-display-h overflow-y-scroll w-full px-5 py-2'>
+            <div className='display-container h-display-h overflow-y-scroll w-full px-5 py-2' ref={displayer}>
                 {
                     props.chatStream.map(i=> <ChatBox key={i.id} data={i}></ChatBox>)
                 }
--- src/components/Workbar/index.tsx
+++ src/components/Workbar/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react"
+import { Ref, RefObject, useContext, useEffect, useState } from "react"
 import { ActionType, AppContext, AppState, ChatStreamItem } from "../../store"
 import ChatBoxFrame from "../../layouts/ChatBoxFrame"
 import ChatDisplay from './ChatDisplay'
@@ -20,25 +20,34 @@ const WorkBar = ()=>{
 
     const handleSubmit = (content:string| undefined)=>{
         const time = new Date().getTime()
-        dispatch({type:ActionType.SUBMIT_MSG,payload:{   
-            from:loginUser,
-            to:state.activedChat,
-            id:'mxmchat_'+time,
-            date:time,
-            type:'text',
-            content:content
-            }
-        })
-        const data = getFriendMsg(state.activedChat)
-        setMsgList(data)
+        if(content){
+            dispatch({type:ActionType.SUBMIT_MSG,payload:{   
+                from:loginUser,
+                to:state.activedChat,
+                id:'mxmchat_'+time,
+                date:time,
+                type:'text',
+                content:content
+                }
+            })
+            const data = getFriendMsg(state.activedChat)
+            setMsgList(data)
+        }
     }
 
+    const handleDisplayer =(item:RefObject<Element>)=>{
+        if(item&&item.current){
+            if(item.current?.clientHeight+item.current?.scrollTop !=  item.current?.scrollHeight){
+               item.current.scrollTop = item.current?.scrollHeight   
+            }
+        } 
+    }
     return(
         <div className="w-full h-full">
             {
                 !state.activedChat?
                 <div className="opacity-30 flex justify-center items-center h-full w-full bg-gray-700">MxM Chat App</div> :
-                <ChatBoxFrame topBar={<ChatDisplay chatStream={msgList}/>} toolsBar={<ChatToolbar textOnsubmit={handleSubmit} />}/>
+                <ChatBoxFrame topBar={<ChatDisplay chatStream={msgList} getDisplayer={handleDisplayer} />} toolsBar={<ChatToolbar textOnsubmit={handleSubmit} />}/>
             }
         </div>
     )
